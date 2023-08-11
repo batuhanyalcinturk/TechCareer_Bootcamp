215edfb4673318fc7a9345569eb3b2fed6bfacf9
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-11 10:53:56 +0300

    JavaScript

---
+++ assists/tutorials/frontend/external.js
@@ -0,0 +1,60 @@
+// alert("deneme12")
+// window.alert("popup");
+// document.writeln("Sayfada <br/>");
+// window.document.writeln("Sayfada 2<br/>");
+
+// console.log("console log");
+// console.error("console log");
+// console.warn("console log");
+// console.info("console log");
+
+var degiskenAdi=44;
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
+
+var degiskenAdi2=23.79;
+console.log(degiskenAdi2);
+console.log(typeof degiskenAdi2);
+
+var $degiskenAdi3="Merhaba canim";
+console.log($degiskenAdi3);
+console.log(typeof $degiskenAdi3);
+
+var _$degiskenAdi3=true;
+console.log(_$degiskenAdi3);
+console.log(typeof _$degiskenAdi3);
+
+// Hoisting
+carName="Audi";
+var carName;
+
+//let
+let deneme =44 ;
+console.log(deneme);
+deneme=66;
+console.log(deneme);
+//const
+// const deneme33=1423;
+// deneme33=23;
+// console.log(deneme33)
+
+// Operatör
+// && = ve  || = Veya   ! Değil
+
+let number1 = 12;
+let number2 = 5;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1/number2);
+console.log(number1*number2);
+console.log(number1%number2);
+
+// = atama
+// == Eşit (Türüne bakmadan)
+// === Eşit (Türüne bakarak)
+
+let userName=Number(prompt("Lütfen sayı giriniz"));
+console.log(userName);
+
+
+
